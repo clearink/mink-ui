577a5a8c1f8c0f860c99b519d8425c8bfc98df45
clearink <42243662+clearink@users.noreply.github.com>
2025-02-17 15:53:32 +0000

    Deploying to gh-pages from @ clearink/mink-ui@10489e8897516710f179b33a107b2179d9cf38c0 🚀

---
+++ assets/vm_5b16bb14b170b9642f06930ac1ee388f-Br1EQ5Ni.js
@@ -0,0 +1,33 @@
+import{j as n}from"./index-DFpG6Uas.js";import{S as e,M as t}from"./index-DUqwrWJ6.js";import{C as o}from"./index-CwZ6dHZw.js";import{A as r}from"./vm_835f7f632162ce0d14730a3854e941f9-Dn8J-xKZ.js";import"./index-B8QgcPLB.js";import"./index-B4818t-L.js";import"./reflow-D11wV1eq.js";function d(){return n.jsxs("div",{className:"source-container",children:[n.jsx(e,{title:"何时使用",children:n.jsx(t,{rawText:"随你"})}),n.jsx(e,{title:"代码演示",children:n.jsx(o,{items:[{desc:{"zh-CN":`基础用法
+
+`,"en-US":`基础用法
+
+\`\`\`tsx
+import { Segmented } from '@mink-ui/core'
+
+export default function App() {
+  return (
+    <Segmented
+      options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']}
+      onChange={(value) => {
+        console.log(value)
+      }}
+    />
+  )
+}
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"segmented-example-Basic",children:n.jsx(r,{})}),rawText:`\`\`\`tsx
+import { Segmented } from '@mink-ui/core'
+
+export default function App() {
+  return (
+    <Segmented
+      options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']}
+      onChange={(value) => {
+        console.log(value)
+      }}
+    />
+  )
+}
+\`\`\`
+`,title:"基础用法"}]})})]})}export{d as default};
---
+++ assets/vm_a8d762a009dc0117cc1dfc7189ee6022-_jDonTyH.js
@@ -0,0 +1,215 @@
+import{j as n}from"./index-DFpG6Uas.js";import{S as e,M as t}from"./index-DUqwrWJ6.js";import{S as r}from"./index-Dj925Zsf.js";import{C as p}from"./index-CwZ6dHZw.js";import{A as i,a as s,b as o,c as a,d as c,e as l}from"./vm_04cb725f08e952600fbef68c8a68c95d-BRUphyuY.js";import"./index-B8QgcPLB.js";import"./index-BCz3h36I.js";import"./flatten-children-Jy6wdQ_q.js";import"./status-DysbMt39.js";import"./index-qrj7RM6o.js";import"./is-null-D6OXYL3N.js";function g(){return n.jsxs("div",{className:"source-container",children:[n.jsx(e,{title:"何时使用",children:n.jsx(t,{rawText:"随你"})}),n.jsx(e,{title:"代码演示",children:n.jsx(p,{items:[{desc:{"zh-CN":`警告提示，展现需要关注的信息，适用于简短的警告提示。
+
+`,"en-US":`警告提示，展现需要关注的信息，适用于简短的警告提示。
+
+xxx en
+
+\`\`\`tsx
+import { Alert } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Alert message="Success Text" type="info" showIcon />
+  )
+}
+
+export default App
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"alert-example-Basic",children:n.jsx(i,{})}),rawText:`\`\`\`tsx
+import { Alert } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Alert message="Success Text" type="info" showIcon />
+  )
+}
+
+export default App
+\`\`\`
+`,title:"基本用法"},{desc:{"zh-CN":`警告提示，展现需要关注的信息，适用于简短的警告提示。
+
+`,"en-US":`警告提示，展现需要关注的信息，适用于简短的警告提示。
+
+xxx en
+
+\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Space direction="vertical">
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert key={type} message={\`\${type} Text\`} type={type} showIcon />
+      ))}
+    </Space>
+  )
+}
+
+export default App
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"alert-example-Type",children:n.jsx(s,{})}),rawText:`\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Space direction="vertical">
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert key={type} message={\`\${type} Text\`} type={type} showIcon />
+      ))}
+    </Space>
+  )
+}
+
+export default App
+\`\`\`
+`,title:"不同类型"},{desc:{"zh-CN":`可关闭
+
+`,"en-US":`xxx en
+
+\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <div>
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          type={type}
+          showIcon
+          closable
+          style={{ marginBottom: 8 }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"alert-example-Closable",children:n.jsx(o,{})}),rawText:`\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <div>
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          type={type}
+          showIcon
+          closable
+          style={{ marginBottom: 8 }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
+\`\`\`
+`,title:"可关闭"},{desc:{"zh-CN":`描述
+
+`,"en-US":`xxx en
+
+\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Space direction="vertical">
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          description="Description text"
+          type={type}
+          showIcon
+        />
+      ))}
+    </Space>
+  )
+}
+
+export default App
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"alert-example-Description",children:n.jsx(a,{})}),rawText:`\`\`\`tsx
+import { Alert, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <Space direction="vertical">
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          description="Description text"
+          type={type}
+          showIcon
+        />
+      ))}
+    </Space>
+  )
+}
+
+export default App
+\`\`\`
+`,title:"含有描述"},{desc:{"zh-CN":"通过 `action` 可以自定义右上角操作项。\n\n","en-US":`通过 \`action\` 可以自定义右上角操作项。
+
+\`\`\`tsx
+import { Alert, Button, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <div>
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          type={type}
+          showIcon
+          closable
+          action={<Button size="small" variant="filled">action</Button>}
+          style={{ marginBottom: 8 }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
+\`\`\`
+`},disabled:!1,element:n.jsx("div",{className:"alert-example-Action",children:n.jsx(c,{})}),rawText:`\`\`\`tsx
+import { Alert, Button, Space } from '@mink-ui/core'
+
+function App() {
+  return (
+    <div>
+      {['info', 'success', 'warning', 'error'].map(type => (
+        <Alert
+          key={type}
+          message={\`\${type} Text\`}
+          type={type}
+          showIcon
+          closable
+          action={<Button size="small" variant="filled">action</Button>}
+          style={{ marginBottom: 8 }}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default App
+\`\`\`
+`,title:"操作项"}]})}),n.jsx(e,{title:"API",children:n.jsx(t,{rawText:`### Alert Props
+
+TODO`})}),n.jsx(e,{title:"Semantic DOM",children:n.jsx(r,{children:n.jsx(l,{})})}),n.jsx(e,{title:"FAQ",children:n.jsx(t,{rawText:`### Q1
+
+xxx
+
+### Q2
+
+xxx`})})]})}export{g as default};
