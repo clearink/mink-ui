755f82827b0f0f92b0e89d156eb82f494f0c874c
clearink <42243662+clearink@users.noreply.github.com>
2025-04-02 10:27:31 +0000

    Deploying to gh-pages from @ clearink/mink-ui@a6f56dff4ec347c8c7fa3eb165ce5bfc23dc2fb2 🚀

---
+++ assets/reflow-CPGZvqZx.js
@@ -0,0 +1,1 @@
+import{M as o}from"./index-CZ9nobDP.js";function e(t){return t?getComputedStyle(t).opacity:o().offsetHeight}export{e as r};
---
+++ assets/vm_2906f92605ff8d919be96e93b6cf1dd1-CA4kKx6W.js
@@ -0,0 +1,9 @@
+import{j as e}from"./index-CZ9nobDP.js";import{S as s,M as a}from"./index-BPXy8e1k.js";import{S as l}from"./index-kPuyCOaj.js";import{C as x}from"./index-BWmid_9a.js";import{i as c,A as n,a as t,b as m,c as i,d as o,e as r,f as p,g as d,h as f,j,k as b}from"./vm_daee22405d2fafd6079b8c68020d3685-Dm3BPS7y.js";import"./index-CGDpRnaX.js";import"./index-BksoE1f5.js";import"./flatten-children-CTuBO8_x.js";import"./status-B3pXMO-n.js";import"./index-B2JWsQ9J.js";import"./is-null-D6OXYL3N.js";function B(){return e.jsxs("div",{className:"source-container",children:[e.jsx(s,{title:"何时使用",children:e.jsx(a,{rawText:"随你"})}),e.jsx(s,{title:"代码演示",children:e.jsx(x,{items:[{desc:c.desc,disabled:!1,element:e.jsx("div",{className:c.cssWrapName,children:e.jsx(n,{})}),rawText:c.rawText,title:"基本用法"},{desc:t.desc,disabled:!1,element:e.jsx("div",{className:t.cssWrapName,children:e.jsx(m,{})}),rawText:t.rawText,title:"不同类型"},{desc:i.desc,disabled:!1,element:e.jsx("div",{className:i.cssWrapName,children:e.jsx(o,{})}),rawText:i.rawText,title:"可关闭"},{desc:r.desc,disabled:!1,element:e.jsx("div",{className:r.cssWrapName,children:e.jsx(p,{})}),rawText:r.rawText,title:"含有描述"},{desc:d.desc,disabled:!1,element:e.jsx("div",{className:d.cssWrapName,children:e.jsx(f,{})}),rawText:d.rawText,title:"操作项"}]})}),e.jsx(s,{title:"API",children:e.jsx(a,{rawText:`### Alert Props
+
+TODO`})}),e.jsx(s,{title:"Semantic DOM",children:e.jsx(l,{children:e.jsx("div",{className:j.cssWrapName,children:e.jsx(b,{})})})}),e.jsx(s,{title:"FAQ",children:e.jsx(a,{rawText:`### Q1
+
+xxx
+
+### Q2
+
+xxx`})})]})}export{B as default};
