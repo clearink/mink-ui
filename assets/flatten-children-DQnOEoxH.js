64012ec08fdccfc78c9987aee614c561dbb96757
clearink <42243662+clearink@users.noreply.github.com>
2025-04-16 09:26:42 +0000

    Deploying to gh-pages from @ clearink/mink-ui@328344eda79dcaff03faa83294f7caf0674cccca 🚀

--- assets/flatten-children-DQnOEoxH.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-f9Y-iN-l.js";import{r as n,i as o,T as p}from"./index-BoMhpNC_.js";function a(s){return n.Children.toArray(s).reduce((t,r)=>o(r)?t:!p.isFragment(r)||!r.props?e(t,r):e(t,a(r.props.children)),[])}export{a as f};
